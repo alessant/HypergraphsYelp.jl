d79cbe69aaebe01ae96aa18302e572343f410ca1
aleant93 <aless.antelmi@gmail.com>
2019-02-12 16:04:26 +0100

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,25 @@
+using Documenter
+
+
+try
+    using HypergraphsYelp
+catch
+    if !("../src/" in LOAD_PATH)
+	   push!(LOAD_PATH,"../src/")
+	   @info "Added \"../src/\"to the path: $LOAD_PATH "
+	   using HypergraphsYelp
+    end
+end
+
+makedocs(
+    sitename = "HypergraphsYelp",
+    format = Documenter.HTML(),
+    modules = [HypergraphsYelp],
+	pages = ["index.md", "reference.md"],
+	doctest = true
+)
+
+deploydocs(
+    repo ="github.com/aleant93/HypergraphsYelp.jl.git",
+	target="build"
+)
