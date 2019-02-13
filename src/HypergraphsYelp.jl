67cb800c7854cbe25ef013b62b1571ad188c5a28
Carmine Spagnuolo <spagnuolocarmine@gmail.com>
2019-02-13 10:29:22 +0100

    init

---
+++ src/HypergraphsYelp.jl
@@ -0,0 +1,12 @@
+__precompile__()
+"""
+    
+"""
+module HypergraphsYelp
+
+using JSON
+using SimpleHypergraphs
+
+include("parser.jl")
+include("analyzer.jl")
+include("builder.jl")
---
+++ test/runtests.jl
@@ -0,0 +1,15 @@
+using Test
+
+const testdir = dirname(@__FILE__)
+
+
+tests = [
+    #"",
+]
+
+@testset "HypergraphsYelp" begin
+    for t in tests
+        tp = joinpath(testdir, "test-$(t).jl")
+        include(tp)
+    end
+end
